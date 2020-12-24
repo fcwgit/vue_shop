38b8b73ae94d6cb53ad525bfe40c4966f50aa400
fcwgit <fcwgit@github.com>
2020-12-24 11:35:51 +0800

    logo 功能

---
+++ src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <el-button type="info" @click="logout">退出</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      a: ''
+    }
+  },
+  methods: {
+    logout () {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+<style lang="less" scope>
+
+</style>
